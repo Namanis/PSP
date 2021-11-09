029f8074f6ed714fa7391beda88f06e002e6a336
Namanis <55447388+Namanis@users.noreply.github.com>
2021-11-09 05:55:15 +0200

    Add files via upload

---
+++ PSP 3/src/test/java/com/example/PSP3/PSP3Application.java
@@ -0,0 +1,13 @@
+package com.example.PSP3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest // gets spring boot context
+class PSP3Application {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
